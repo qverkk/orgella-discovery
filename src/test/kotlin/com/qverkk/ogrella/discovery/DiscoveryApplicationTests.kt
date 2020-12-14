7b9654a3c105caa2b702e671210aaea5233aa8c9
qverkk <membersy@gmail.com>
2020-12-14 19:44:36 +0100

    Add init files

---
+++ src/test/kotlin/com/qverkk/ogrella/discovery/DiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.qverkk.ogrella.discovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
